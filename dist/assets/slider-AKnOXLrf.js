f1d75b2a90d74909f9336ab9cfc8adcb5a26161f
greenhollow <>
2024-05-21 16:45:25 +0000

    Update from https://github.com/greenhollow/webzen-projects-dev/commit/e9a1aba6132739e27bbbfbacdf7f2ea2cde0cc2b

---
+++ dist/assets/slider-AKnOXLrf.js
@@ -0,0 +1,7 @@
+import{d as e,e as s,_ as a}from"./app-dHyqPXmv.js";const p=e((({app:e})=>{e.component("Slider",s({loader:()=>a((()=>import("./slider-UNTv4rVV.js")),__vite__mapDeps([0,1,2])).then((e=>e.default||e)),delay:0,suspensible:!1}))}));export{p as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/slider-UNTv4rVV.js","assets/app-dHyqPXmv.js","assets/index-qL7jkcSn.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
