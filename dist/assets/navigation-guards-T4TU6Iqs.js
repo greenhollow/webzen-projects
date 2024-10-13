69d92c3757c98d31560a54d2b46a13535d451874
greenhollow <>
2024-10-13 19:56:43 +0000

    Update from https://github.com/greenhollow/webzen-projects-dev/commit/7d237679865f0042423eb39a973df5b2cfbe1295

---
+++ dist/assets/navigation-guards-T4TU6Iqs.js
@@ -0,0 +1,1 @@
+import{d as a}from"./app-XC-zDgSz.js";const o=a((({router:a})=>{a.beforeEach((async(a,o)=>{}))}));export{o as default};
---
+++ dist/assets/slider-tgqsvghE.js
@@ -0,0 +1,7 @@
+import{d as e,e as s,_ as a}from"./app-XC-zDgSz.js";const p=e((({app:e})=>{e.component("Slider",s({loader:()=>a((()=>import("./slider-mYRAApJq.js")),__vite__mapDeps([0,1,2])).then((e=>e.default||e)),delay:0,suspensible:!1}))}));export{p as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/slider-mYRAApJq.js","assets/app-XC-zDgSz.js","assets/index-s5gK1l-p.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
